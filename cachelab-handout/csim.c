7cd2f2cee0f02eaf8ad55ee0735e4a9d4a49ae1b
Jason Li <2594167452@qq.com>
2024-09-09 21:10:08 +0800

    Complete command-line function. next todo: trace file read

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -4,25 +4,20 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void printInfo();
-
+void print_info();
+void read_trace(FILE* fp); 
 int main(int argc, char* argv[]) {
 	int opt;
 	int s, E, b = 0;
-	char * trace_file = NULL;
+	char* trace_file = NULL;
+	FILE* fp = NULL;
 	while(opt = getopt(argc, argv, "hvs:E:b:t:") != -1) {
 		switch(opt) {
 			case 'h':
-				printf("Help:\n
-						\t-h: see help\n
-						\t-v: display traces info\n
-						\t-s: number of sets\n
-						\t-E: number of lines in a set\n
-						\t-b: number of bytes in a line\n
-						\t-t: trace file to test\n");
+				printf("Help:\n\t-h: see help\n\t-v: display traces info\n\t-s: number of sets\n\t-E: number of lines in a set\n\t-b: number of bytes in a line\n\t-t: trace file to test\n");
 				break;
 			case 'v':
-				printInfo();
+				print_info();
 				break;	
 			case 's':
 				s = atoi(optarg);
@@ -35,8 +30,17 @@ int main(int argc, char* argv[]) {
 				break;
 			case 't':
 				trace_file = optarg;	
+				break;
+			default:
+				printf("Bad command line ;(\n");
+				break;
 		}
 	}
+	
+	fp = fopen(trace_file, "r");
+
+	read_trace(fp);
+
     printSummary(0, 0, 0);
     return 0;
 }
